11f0dfd381c1a245b330e262b5442dc80d4c0f3d
Kudiser <jamens1310@gmail.com>
2024-11-15 18:47:53 -0500

    Create botcommand.py

---
+++ botcommand.py
@@ -0,0 +1,49 @@
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+botsito=commands.Bot(command_prefix="/", intents=intents)
+
+@botsito.event
+async def on_ready():
+    print(f"se inicio el bot {botsito.user}")
+
+@botsito.command()
+async def hola(ctx):
+    await ctx.send("Hola, como estas")
+
+@botsito.command()
+async def suma(ctx,num1:int,num2:int):
+    suma=num1+num2
+    await ctx.send(f"el resultado de {num1} + {num2} = {suma}")
+
+@botsito.command()
+async def randomN(ctx,longitud: int):
+    if longitud < 0:
+        await ctx.send("La longitud de tu numero no puede ser menor a 0")
+        return
+        
+    numero_al_azar = random.randint(10**(longitud - 1), 10**longitud - 1)
+    await ctx.send(f"Tu número aleatorio de {longitud} dígitos es: {numero_al_azar}")
+
+@botsito.command()
+async def resta(ctx,num1:int,num2:int):
+    resta=num1-num2
+    await ctx.send(f"el resultado de {num1} - {num2} = {resta}")
+
+@botsito.command()
+async def multiplica(ctx,num1:int,num2:int):
+    multiplica=num1*num2
+    await ctx.send(f"el resultado de {num1} * {num2} = {multiplica}")
+
+@botsito.command()
+async def divide(ctx,num1:int,num2:int):
+    divide=num1/num2
+    await ctx.send(f"el resultado de {num1} / {num2} = {divide}")
+
+
+
+botsito.run("MTI5NzMwMjQ5MTEyNTM4MzI1MA.G77OqC.-dZBi9KVo3SEBuRes69_oaI4XAkHuNSoery4rg")
